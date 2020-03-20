4a61ada131ce4df667e2c9279cd081c97ce32f65
Michael-Kamau <kamau.karitu@gmail.com>
2020-03-20 15:13:41 +0300

    Inititial commit from app instantiation

--- __tests__/Home-test.js
+++ __tests__/Home-test.js
@@ -3,8 +3,9 @@ import React from 'react';
 import Home from '../src/components/Home'
 import renderer from 'react-test-renderer'
 
+
 test('Home snapshot',()=>{
-    const snap= renderer.create(
+    const snap= renderer.create.create(
         <Home />
     ).toJSON();
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,12 @@
 import React,{Component} from 'react';
-import {View,Text,TextInput}  from 'react-native';
-import Profile from './Profile';
+import {View,Text}  from 'react-native';
+
+
 class Home extends Component{
-
-    constructor(){
-        super();
-        this.state={
-            data:"test",
-        }
-    }
-
-
-
-
-    change(x){
-        this.setState({data:x*5})
-    }
-
-
     render(){
         return(
             <View>
                 <Text>Home Component</Text>
-                <Profile/>
             </View>
         )
     }
