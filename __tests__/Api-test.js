13cab23a383b2c85a9059a8020a9891a87381a08
Michael-Kamau <kamau.karitu@gmail.com>
2020-03-20 21:01:54 +0300

    (Video 5) - Testing API without mock function(using a simple example)

--- __tests__/Api-test.js
+++ __tests__/Api-test.js
@@ -1,20 +1,25 @@
 import Users from '../src/components/Users';
+import 'isomorphic-fetch';
 
 it('Api test case', async function () {
-    global.fetch = jest.fn().mockImplementation(()=>
-    {
-        let p = new Promise((resolve, reject) => {
-            resolve({
-                json: function () {
-                    return {Id: 1};
-                }
-            })
-        })
+    // global.fetch = jest.fn().mockImplementation(()=>
+    // {
+    //     let p = new Promise((resolve, reject) => {
+    //         resolve({
+    //             json: function () {
+    //                 return {Id: 1};
+    //             }
+    //         })
+    //     })
+    //
+    //     return p
+    // })
 
-        return p
-    })
+    // const response = await Users.all();
+    // expect(response.Id).toBe(1);
 
-    const response=await Users.all();
 
-    expect (response.Id).toBe(1)
+    const response = await Users.all();
+    console.warn(response.movies[2].title);
+    expect(response.movies[2].title).toEqual('The Matrix');
 });
