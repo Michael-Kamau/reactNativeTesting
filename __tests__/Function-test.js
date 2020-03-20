c45711f9a39500fd957054b289c782b0f2c11cef
Michael-Kamau <kamau.karitu@gmail.com>
2020-03-20 16:52:08 +0300

    Functions test

---
+++ __tests__/Function-test.js
@@ -0,0 +1,17 @@
+import 'react-native'
+import React from 'react'
+import Home from '../src/components/Home';
+
+import renderer from 'react-test-renderer';
+
+it('check function and state test case',()=>{
+    let HomeData =renderer.create(<Home/>).getInstance();
+
+    HomeData.change(2)
+    // console.log(HomeData)
+
+    // expect(HomeData.change(2)).toEqual(20 )
+    expect(HomeData.state.data).toEqual(20 )
+
+
+})
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,28 @@
 import React,{Component} from 'react';
-import {View,Text}  from 'react-native';
+import {View,Text,TextInput}  from 'react-native';
+import Profile from './Profile';
 
 
 class Home extends Component{
+
+    constructor(){
+        super();
+        this.state={
+            data:"test",
+        }
+    }
+
+    change(x){
+        // return x*10;
+
+        this.setState({data:x*10})
+
+    }
     render(){
         return(
             <View>
                 <Text>Home Component</Text>
+                <Profile/>
             </View>
         )
     }
