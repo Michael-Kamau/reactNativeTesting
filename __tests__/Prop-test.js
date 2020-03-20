a2e0a299187eec7612b1f01f2ee7e8943d11f88e
Michael-Kamau <kamau.karitu@gmail.com>
2020-03-20 21:37:34 +0300

    (Video 6) - Testing props

---
+++ __tests__/Prop-test.js
@@ -0,0 +1,17 @@
+import 'react-native'
+import React from 'react'
+import Profile from '../src/components/Profile';
+import renderer from 'react-test-renderer'
+
+import {configure,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+it('Testing props',()=>{
+    const wrapper=shallow(<Profile data="Hello"/>).props();
+
+    console.warn(wrapper.children.props.children)
+
+    expect(wrapper.children.props.children).toEqual("Hello")
+})
